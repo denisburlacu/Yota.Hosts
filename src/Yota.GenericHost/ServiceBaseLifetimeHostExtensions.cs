dccba929063f5374211ec83e58624d3a0fba6bf2
denisburlacu <burlacudenys@yahoo.com>
2022-03-28 20:53:22 +0300

    Initial commit

---
+++ src/Yota.GenericHost/ServiceBaseLifetimeHostExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Yota.GenericHost
+{
+    public static class ServiceBaseLifetimeHostExtensions
+    {
+        public static IHostBuilder UseServiceBaseLifetime(this IHostBuilder hostBuilder)
+        {
+            return hostBuilder.ConfigureServices((hostContext, services) => services.AddSingleton<IHostLifetime, ServiceBaseLifetime>());
+        }
+
+        public static Task RunAsServiceAsync(this IHostBuilder hostBuilder,
+            CancellationToken cancellationToken = default)
+        {
+            var runServiceTask = hostBuilder.UseWindowsService()
+                .UseServiceBaseLifetime()
+                .Build()
+                .RunAsync(cancellationToken);
+
+            return runServiceTask;
+        }
+    }
+}
